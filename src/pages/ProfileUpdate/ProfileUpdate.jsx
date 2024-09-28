bff6d8160c11f1d9a062a5ff519ce8632e781f17
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-09-28 12:50:00 +0530

    componenst done

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -9,21 +9,32 @@ const profileUpdate = () => {
   return (
     <div className="profile">
       <div className="profile-container">
-        <form >
+        <form>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
-            <input onChange={() => setImage(e.target.files[0])} type="file"  id="avatar" accept='.png, .jpeg, jpg' hidden />
-            <img src={image? URL.createObjectURL(image): assets.avatar_icon}  />
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpeg, jpg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+            />
             update image profile
           </label>
-          <input type="text" placeholder='your name' required />
-          <textarea placeholder='write profile bio'></textarea>
-          <button type='submit'>Save</button>
+          <input type="text" placeholder="your name" required />
+          <textarea placeholder="write profile bio"></textarea>
+          <button type="submit">Save</button>
         </form>
-        <img className='profile-pic' src={assets.logo_icon}  />
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+        />
       </div>
     </div>
-  )
+  );
 }
 
 export default profileUpdate
