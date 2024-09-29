ff4701767bf8e655e0a8852f02a93bb6679623e6
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-09-29 10:36:59 +0530

    creating api

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/login/login'
-import Chat from './pages/chat/chat'
-import Profile from './pages/profileUpdate/profileUpdate'
-
-
-
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./pages/login/login";
+import Chat from "./pages/chat/chat";
+import Profile from "./pages/profileUpdate/profileUpdate";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
 
 const App = () => {
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate('/chat')
+      }else{
+        navigate('/')
+      }
+    })
+  }, [])
+
   return (
     <>
-     <Routes>
-      <Route path='/' element= {<Login/>}/>
-      <Route path='/chat' element= {<Chat/>}/>
-      <Route path='/profile' element= {<Profile/>}/>
-     </Routes>
+      <ToastContainer/>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
