359701361b21b7fd139c8d963ed1a37771167cd1
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-09-29 10:37:51 +0530

    app context

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,32 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
 import "./login.css";
 import assert from "../../assets/assets.js";
+import { singup, loginUser, logOut } from "../../config/firebase.js";
 
 const login = () => {
+  const [currState, setCurrState] = useState("Sing up");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [currState, setCurrState] = useState("Sing up")
-
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (currState === "Sing up") {
+      singup(username, email, password);
+    } else {
+      loginUser(email, password);
+    }
+  };
 
   return (
     <div className="login">
       <img src={assert.logo_big} className="logo" />
-      <form className="login-form">
+      <form onSubmit={submitHandler} className="login-form">
         <h2>{currState}</h2>
         {currState === "Sing up" ? (
           <input
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
             type="text"
             className="form-input"
             placeholder="Username"
@@ -23,12 +34,16 @@ const login = () => {
           />
         ) : null}
         <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
           type="email"
           className="form-input"
-          placeholder={currState === "Login" ? "Username or Email" : "Email"}
+          placeholder="Email"
           required
         />
         <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
           type="password"
           className="form-input"
           placeholder="Password"
@@ -38,18 +53,19 @@ const login = () => {
           {currState === "Sing up" ? "Sing up" : "Login"}
         </button>
         <div className="login-term">
-          <input type="checkbox" required />
+          <input type="checkbox" />
           <p>Agree to terms of use & privacy policy.</p>
         </div>
         <div className="login-forgot">
           <p className="login-toggle">
             {currState === "Sing up"
-              ? "Already have a account": "Create new account"}
+              ? "Already have a account"
+              : "Create new account"}
             <span
               onClick={() => {
                 if (currState === "Sing up") {
                   setCurrState("Login");
-                }else{
+                } else {
                   setCurrState("Sing up");
                 }
               }}
